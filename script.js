5132593dff819ad1058d7f4988a49a54d4f79954
jdc9111 <52749095+jdc9111@users.noreply.github.com>
2020-08-09 14:32:08 -0400

    version 1 before adding node

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 
 
+console.log('it works');
+
+const sendButton = document.querySelector('.sendButton');
+const mainForm=document.querySelector('.mainForm');
+
+
+
+function handleSend(e) {
+    e.preventDefault();
+    
+    console.log ('Sending!');
+
+    phoneNumber = document.querySelector('.sendNumber').value;
+    console.log(phoneNumber);
+
+    handout = document.querySelector('.handoutToSend').value;
+    console.log(handout);
+
+    // show alert
+    document.querySelector('.alert').style.display = 'block';
+
+    // hide alert after 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').style.display='none';
+    },3000);
+
+};
+
+
+
+mainForm.addEventListener('submit', handleSend);
+
+
+
+
